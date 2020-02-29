30b09e4254151cd02531e7584bffa8cc3a304b17
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-29 19:22:05 +0100

    Simplify the service method arguments

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -8,26 +8,28 @@ class Auth {
     });
   }
 
-  signup(user) {
-    const { username, password } = user;
+  signup({ username, password }) {
     return this.auth
       .post("/auth/signup", { username, password })
       .then(({ data }) => data);
+    // .then((response) => response.data);
   }
 
-  login(user) {
-    const { username, password } = user;
+  login({ username, password }) {
     return this.auth
       .post("/auth/login", { username, password })
       .then(({ data }) => data);
+    // .then((response) => response.data);
   }
 
   logout() {
-    return this.auth.post("/auth/logout", {}).then( (response) => response.data);
+    return this.auth.post("/auth/logout", {}).then(({ data }) => data);
+    // return this.auth.post("/auth/logout", {}).then((response) => response.data);
   }
 
   me() {
-    return this.auth.get("/auth/me").then( (response) => response.data);
+    return this.auth.get("/auth/me").then(({ data }) => data);
+    // return this.auth.get("/auth/me").then((response) => response.data);
   }
 }
 
